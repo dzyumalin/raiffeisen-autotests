5e1dfe21886b8faf6c51e7549e3cf2d802d05745
dmitryhli <zyumalin@bk.ru>
2021-06-29 04:01:00 +0300

    add find test

---
+++ src/test/java/ru/raiffeisen/helpers/Logging.java
@@ -0,0 +1,8 @@
+package ru.raiffeisen.helpers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Logging {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Logging.class);
+}
