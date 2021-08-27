b13563ffbd5b803e23dc4219c0351ffa46dcac52
Zyumalin Dmitriy <zyumalin@bk.ru>
2021-08-27 20:20:22 +0300

    added annotation in api test

--- src/test/java/ru/raiffeisen/tests/api/tests/ReqresinTests.java
+++ src/test/java/ru/raiffeisen/tests/api/tests/ReqresinTests.java
@@ -1,15 +1,27 @@
 package ru.raiffeisen.tests.api.tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.raiffeisen.annotations.Layer;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
 import static ru.raiffeisen.tests.api.Specs.request;
 import static ru.raiffeisen.tests.api.Specs.responseSpec;
 
+@Owner("Dmitriy")
+@Layer("Api")
+@Feature("Reqres")
 public class ReqresinTests {
 
     @Test
+    @Tag("Api")
+    @Description("Main page")
+    @DisplayName("Get and check list user data")
     public void listResource() {
         given()
                 .spec(request)
